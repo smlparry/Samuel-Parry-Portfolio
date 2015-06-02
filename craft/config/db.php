db0cd7e68a0a1fa55f63be3b778a77f07669acc7
smlparry <samuel.parry@bigpond.com>
2015-06-02 14:11:20 +1000

    font from google fonts

--- craft/config/db.php
+++ craft/config/db.php
@@ -11,18 +11,9 @@ return array(
 
 	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
 	'server' => 'localhost',
-
-	'.dev' => array(
-        'user' => 'root',
-        'password' => 'root',
-        'database' => 'samuelparry',
-    ),
-    '.com' => array(
-        'user' => 'forge',
-        'password' => 'SygU68gppdfKah88YBXw',
-        'database' => 'samuelparry',
-    ),
-
+  'user' => 'homestead',
+  'password' => 'secret',
+  'database' => 'samuelparry',
 	// The prefix to use when naming tables. This can be no more than 5 characters.
 	'tablePrefix' => 'craft',
 
